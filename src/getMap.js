c52457865e97ca446c709d09731a77ff9c742bf7
Ivens_Zhang <ks_zhangyi@163.com>
2021-03-15 09:37:06 +0800

    chore: add some comment

--- src/getMap.js
+++ src/getMap.js
@@ -1,13 +1,15 @@
 const fs = require('fs'); //引入文件读取模块
 const request = require('request');
 
-
+// ================== Config =========================================
+// 图片保存到的地址
 const dir = `assets/map_en`
 // 爬取层级, 每级的横纵块数
 const level = 6
 // 地图配置相关
 let imgUrlRoot = `https://api.mapbox.com/styles/v1/meroychen/ckiod6ao60b0p17sg086wtnb4/tiles/512`
 const token = `pk.eyJ1IjoibWVyb3ljaGVuIiwiYSI6ImNraTc2NTR2ejRlNGQyeHJtc29jcTJ4ZXUifQ.xrCXPnyPfIuxHpXXdmcPMQ`
+// ================== Config =========================================
 
 // 错误处理
 const catchErr = err => {
